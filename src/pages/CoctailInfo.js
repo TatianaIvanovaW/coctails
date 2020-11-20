0ba8f216a14ee8022860da0aa2df21a43030f4aa
Tatiana Ivanova <ivanova.tatiana.w@gmail.com>
2020-11-20 13:30:21 +0100

    add alt to img

--- src/pages/CoctailInfo.js
+++ src/pages/CoctailInfo.js
@@ -4,7 +4,7 @@ export default function CoctailInfo(props) {
   return (
     <div>
       <h1>{props.name}</h1>
-      <img src={props.img}></img>
+      <img src={props.img} alt="coctail"></img>
       <h3> how to make:</h3>
       <p>{props.make}</p>
     </div>
